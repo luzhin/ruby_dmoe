1ca5b79dca871c3289080637a096d76b00f52c2b
luzhin <luzhin.kirill@yandex.ru>
2015-01-04 15:12:36 +0200

    DMOE. 1.1.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'store/index'
+
   resources :products
 
   get 'say/hello'
@@ -8,6 +10,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root to: 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

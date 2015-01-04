53d20cd3ce6e9a227fe414123e3082272c956199
luzhin <luzhin.kirill@yandex.ru>
2015-01-04 15:05:42 +0200

    DMOE. 1.0.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence:true
   validates :price, numericality: {greater_than_or_equal_to:0.01}
   validates :title, uniqueness:true
+  validates :title, length: {minimum: 10, message:'Название должно быть не менее 10 символов.'}
   validates :image_url, allow_blank:true, format:{
     with: %r{\.(gif|jpg|png)$}i,
     :multiline => true,
